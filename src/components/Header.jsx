50eb776e63d5e1561b087f2674372e4295825c36
ofekats <ofekats@gmail.com>
2025-06-08 13:27:24 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header(){
+    return (
+        <header>
+            <img className="header-logo" src="../images/chef-claude-icon.png" alt="chef-claude-icon"/>
+            <h1 className="header-text">Chef Claude</h1>
+        </header>
+    )
+}
