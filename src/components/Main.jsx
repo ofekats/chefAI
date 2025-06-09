b646ccc2098f2070e93fe8258f591717e7c25aba
ofekats <ofekats@gmail.com>
2025-06-09 14:54:34 +0300

    adding useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react"
+
 export default function Main() {
+
+
+    const [ingredients, setIngredients] = useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prev => [...prev, newIngredient])
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input 
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
