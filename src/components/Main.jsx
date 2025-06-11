f20f5719bbf053b4f8a01df94e1d420455e943a8
ofekats <ofekats@gmail.com>
2025-06-11 14:03:26 +0300

    action instead of onSubmit for the form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,16 +9,14 @@ export default function Main() {
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prev => [...prev, newIngredient])
     }
 
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
