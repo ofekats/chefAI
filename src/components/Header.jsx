3b843df5b03ac63b522709d648446e0715d7f721
ofekats <ofekats@gmail.com>
2025-06-19 13:24:35 +0300

    images folder to public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header(){
     return (
         <header>
-            <img className="header-logo" src="../public/images/chef-icon.png" alt="chef-icon"/>
+            <img className="header-logo" src="../images/chef-icon.png" alt="chef-icon"/>
             <h1 className="header-text">Chef AI</h1>
         </header>
     )
